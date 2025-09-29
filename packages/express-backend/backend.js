6df2bdad57e4f45d682fbd7e44807d3082c78f94
brianthich <thichbrian@gmail.com>
2025-09-29 16:07:48 -0700

    Seventh Commit

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -2,47 +2,56 @@ import express from "express";
 
 const app = express();
 const port = 8000;
+app.use(express.json());
+
 const users = {
   users_list: [
     {
       id: "xyz789",
       name: "Charlie",
-      job: "Janitor"
+      job: "Janitor",
     },
     {
       id: "abc123",
       name: "Mac",
-      job: "Bouncer"
+      job: "Bouncer",
     },
     {
       id: "ppp222",
       name: "Mac",
-      job: "Professor"
+      job: "Professor",
     },
     {
       id: "yat999",
       name: "Dee",
-      job: "Aspring actress"
+      job: "Aspring actress",
     },
     {
       id: "zap555",
       name: "Dennis",
-      job: "Bartender"
-    }
-  ]
+      job: "Bartender",
+    },
+  ],
 };
 
+// Search Methods
 const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
+  return users["users_list"].filter((user) => user["name"] === name);
 };
 
-const findUserById = (id) =>
+const findUserById = (id) => {
   users["users_list"].find((user) => user["id"] === id);
+};
 
+// User Addition
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+// Get Methods
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; 
+  const id = req.params["id"];
   let result = findUserById(id);
   if (result === undefined) {
     res.status(404).send("Resource not found.");
@@ -66,14 +75,17 @@ app.get("/users", (req, res) => {
   res.send(users);
 });
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+  console.log(`Example app listening at http://localhost:${port}`);
 });
